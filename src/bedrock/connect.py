df7ae2092893a74e7993974e099ea1cfe57e38b7
kumaotto <kwkomaki.kom@gmail.com>
2024-06-04 03:55:10 +0900

    add a system prompt

--- src/bedrock/connect.py
+++ src/bedrock/connect.py
@@ -17,24 +17,24 @@ def generate_message(bedrock, system_prompt: str, messages: str) -> str:
     :return: Bedrockからのレスポンス
     """
 
-    body = json.dumps(
+    body: dict = json.dumps(
         {
             'anthropic_version': 'bedrock-2023-05-31',
             'max_tokens': MAX_TOKENS,
-            # 'system_prompt': system_prompt, # システムプロンプトを指定する場合はコメントアウトを解除
+            'system_prompt': system_prompt,
             'messages': messages
         }
     )
 
-    accept = 'application/json'
-    contentType = 'application/json'
+    accept: str = 'application/json'
+    contentType: str = 'application/json'
     response = bedrock.invoke_model(body=body, modelId=MODEL_ID, accept=accept, contentType=contentType)
-    response_body = json.loads(response.get('body').read())
+    response_body: dict = json.loads(response.get('body').read())
 
     return response_body["content"][0]["text"]
 
 
-def execute_bedrock_api(message: str = None) -> None:
+def execute_bedrock_api(message: str) -> str:
 
     """
     Bedrock APIを実行する
@@ -47,21 +47,35 @@ def execute_bedrock_api(message: str = None) -> None:
         # バージニアリージョンのBedrockランタイムクライアントを作成
         bedrock = boto3.client('bedrock-runtime', region_name = "us-east-1")
 
-        system_prompt: str = ''
+        system_prompt: str = '''
+        
+        あなたは通信士です。あなたは、モールス符号を使ってメッセージを送信する訓練を受けています。
+        今、あなたは、モールス符号をデコードされたメッセージを受信しました。デコードされたメッセージを自然言語に変換し、その内容に基づいて返信してください。
+        但し、返信は和文モールス符号に変換可能なテキスト、かつ10文字以内である必要があります。
+
+        <task>
+        返信を和文モールス符号に変換可能なテキストで返信してください。
+
+        以下は出力例です。
+        <example id="1">
+        けんきてすか
+        </example>
+
+        <example id="2">
+        りようかい
+        </example>
+
+        </task>
+        '''
 
         # サンプルメッセージでBedrock APIを実行
-        user_message: dict = {'role': 'user', 'content': '味噌汁の作り方を説明してください'}
+        user_message: dict = {'role': 'user', 'content': message}
         messages: list[str] = [user_message]
 
-        response = generate_message(bedrock, system_prompt, messages)
-        print(response)
-
+        response: str = generate_message(bedrock, system_prompt, messages)
+        
+        return response
 
     except Exception as e:
         print('Error:', e)
         return
-
-
-if __name__ == "__main__":
-    execute_bedrock_api()
-    
--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
+from bedrock.connect import execute_bedrock_api
 from morse.decode import decode_morse
 from morse.input import get_key_info
 
 
 if __name__ == '__main__':
-    print('start morse code decoder...')
+    print('モールス符号を入力してください。escキーで終了します。')
     morse_code = get_key_info()
     print('morse_code:', morse_code)
     result = decode_morse(morse_code)
     print('result:', result)
+    bedrock_result = execute_bedrock_api(result)
+    print('bedrock_result:', bedrock_result)
